2fa1f78aecd7dfcfa308423bae57d72f13ab8e47
Your Name <your.email@example.com>
2024-08-04 14:29:51 +0300

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'di.dart';
@@ -6,10 +7,18 @@ import 'ui/screen/auth/register/Register.dart';
 import 'ui/screen/mian/main.dart';
 import 'ui/screen/mian/tabs/home/proudect-detials/proudect-details.dart';
 import 'ui/screen/splash/splash.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'ui/utils/app_theme.dart';
 
 
-void main() {
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: const FirebaseOptions(
+      apiKey: "AIzaSyDFOaRG9gm5pNGKGL9pB8sLBzsrm07ZcZI",
+      appId: "e-commerce-4b3cc",
+      messagingSenderId: "e-commerce-4b3cc",
+      projectId: "e-commerce-4b3cc"));
   configureDependencies();
   runApp(const MyApp());
 }
--- lib/ui/screen/mian/mainviewmodel/main_view_model.dart
+++ lib/ui/screen/mian/mainviewmodel/main_view_model.dart
@@ -1,13 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
+import '../../../../model/prouductresponse.dart';
 import 'main-view-model-state.dart';
 
 @injectable
 class MainViewModel extends Cubit<MainScreenState> {
   MainViewModel() : super(MainScreenState());
 
+
   void setcurrenttabindex(int newindex) {
     emit(state.copyWith(currentTabIndex: newindex));
   }
+
+
+
+  Future<ProudecResponse> getProduct() async {
+    CollectionReference collectionReference =
+    FirebaseFirestore.instance.collection("fav");
+    DocumentSnapshot documentSnapshot =
+    await collectionReference.doc().get();
+    Map<String,dynamic> json = documentSnapshot.data() as Map<String,dynamic>;
+    return ProudecResponse.fromJson(json);
+  }
+
+
 
 }
